a315fd29234656b968292dbea2d14b5f338006dd
Octaviop15 <octaviopic15@gmail.com>
2018-04-12 00:49:14 -0300

    agregando clases

---
+++ src/Modelo/Libro.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Octavio
+ */
+public class Libro {
+    private int ISBN;
+    private String titulo;
+    private String fechaLanzamiento;
+    private int paginas;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private Autor autor;
+    private Editorial editorial;
+    private Categoria categoria;
+    
+}
